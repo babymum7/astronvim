f03a0f2f34b5d4e3243f69b5159ef199d374905e
babymum7 <nbtrong2312@gmail.com>
2022-08-03 13:58:27 +0700

    add config for go

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
+local util = require "lspconfig/util"
+
 local config = {
+  colorscheme = "gruvbox",
+  options = {g = {go_metalinter_command = "golangci-lint"}},
   plugins = {
+    init = {{"fatih/vim-go"}, {"ellisonleao/gruvbox.nvim"}},
     ["neo-tree"] = function(config)
       config.window.width = 30
       config.filesystem.filtered_items.hide_dotfiles = false
@@ -12,11 +17,23 @@ local config = {
       config.sources = {
         null_ls.builtins.formatting.lua_format.with({
           extra_args = {"--tab-width", "2", "--indent-width", "2"}
-        })
+        }), null_ls.builtins.diagnostics.golangci_lint
       }
 
       return config
     end
+  },
+  lsp = {
+    ["server-settings"] = {
+      gopls = {
+        cmd = {"gopls", "serve"},
+        filetypes = {"go", "gomod"},
+        root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+        settings = {
+          gopls = {gofumpt = true, experimentalWorkspaceModule = true}
+        }
+      }
+    }
   }
 }
 
